7166e4994c12a009d6b3db5e7bc168612fcf9da2
whitequark <whitequark@whitequark.org>
2026-08-08 20:47:04 +0200

    Make the command prompt banner prettier.

--- wix/res/vars.bat
+++ wix/res/vars.bat
@@ -1,4 +1,9 @@
 @set PATH=%~dp0bin;%PATH%
 @cd /d %USERPROFILE%
 @title Glasgow Interface Explorer Command Prompt
-@echo Quickstart: run "glasgow --help"
+@echo *************************************************
+@echo **  Glasgow Interface Explorer Command Prompt  **
+@echo **  Quickstart: run "glasgow --help"           **
+@echo *************************************************
+@echo.
+@glasgow --version
