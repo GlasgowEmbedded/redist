01b59ac8939179323a07a9e34b40bb7d6d5b1dfa
whitequark <whitequark@whitequark.org>
2026-08-09 01:31:20 +0200

    Add `%USERPROFILE%\.local\bin` to `PATH`.
    This is where `python -m pip install ...` places binary entry points.

--- wix/res/vars.bat
+++ wix/res/vars.bat
@@ -1,4 +1,4 @@
-@set PATH=%~dp0bin;%PATH%
+@set PATH=%~dp0bin;%USERPROFILE%\.local\bin;%PATH%
 @cd /d %USERPROFILE%
 @title Glasgow Interface Explorer Command Prompt
 @echo *************************************************
