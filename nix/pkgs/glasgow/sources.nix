08c95626aaf1f63d4978f6ae2af89b49cbd38feb
Catherine <whitequark@whitequark.org>
2026-08-16 13:38:17 +0000

    Update to latest Glasgow.

--- nix/pkgs/glasgow/sources.nix
+++ nix/pkgs/glasgow/sources.nix
@@ -3,6 +3,6 @@
 fetchFromGitHub {
   owner = "GlasgowEmbedded";
   repo = "glasgow";
-  rev = "02e1f9019d945bf778f2986721349529ceeaeedd";
-  hash = "sha256-lmQWbukzkh1pk0ip1hZ+f7dONw6Csuqohpbl8cIO9zk=";
+  rev = "dd3bf7e48ce883e17b596d406efeb818f28fd9c4";
+  hash = "sha256-jbjv/2Uuowihmd+dpypt22U8Yd6YuIe7H+xm8iqAhPA=";
 }
