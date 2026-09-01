32a5f528f72a44a8bb15c6b954a9781067f7d818
bin <bin@sleepy.rs>
2026-09-02 00:44:35 +0100

    Install a copy of pip with the rest of the wheels.
    This creates a `pip` alias so users don't need to do `python -m pip`,
    and reduces potential confusion if `pip` is already present on the
    path from a different Python installation.

--- nix/pkgs/glasgow/wheels.nix
+++ nix/pkgs/glasgow/wheels.nix
@@ -1,5 +1,6 @@
 {
   callPackage,
+  fetchurl,
   glasgowPkgs,
   runCommand,
 }:
@@ -8,6 +9,12 @@ let
   glasgow-sources = callPackage ./sources.nix { };
 
   wheels = glasgowPkgs.render { filename = "${glasgow-sources}/software/pdm.dist.lock"; };
+
+  pip-filename = "pip-26.2.1-py3-none-any.whl";
+  pip = fetchurl {
+    url = "https://files.pythonhosted.org/packages/py3/p/pip/${pip-filename}";
+    hash = "sha256-cROK3x9MqQDNt9KJwht0lDKfIzK22F8OHEIQjAOE7T4=";
+  };
 in
 runCommand "wheels" { } ''
   mkdir $out
@@ -17,4 +24,7 @@ runCommand "wheels" { } ''
 
   # Glasgow dependencies.
   cp ${wheels}/*.whl $out
+
+  # Newer version of pip.
+  cp ${pip} $out/${pip-filename}
 ''
